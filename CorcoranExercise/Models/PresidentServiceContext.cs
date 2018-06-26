9464dc2b0514d622de4c327cedca577dd297fb55
Alan Gonzalo Ramos <alanramos_18@hotmail.com>
2018-06-26 04:28:23 -0300

    Done

---
+++ CorcoranExercise/Models/PresidentServiceContext.cs
@@ -0,0 +1,55 @@
+﻿using OfficeOpenXml;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+
+namespace CorcoranExercise.Models
+{
+    public class PresidentServiceContext
+    {
+        public DataSet DtSet;
+        OleDbConnection MyConnection;
+        OleDbDataAdapter MyCommand;
+
+        static string FilePath = HostingEnvironment.ApplicationPhysicalPath + "\\Utilities\\Presidents.xlsx";
+
+        public PresidentServiceContext()
+        {
+            DtSet = new DataSet();
+
+            DtSet.Tables.Add(GetDataTableFromExcel(FilePath));
+        }
+
+        private static DataTable GetDataTableFromExcel(string filePath, bool hasHeader = true)
+        {
+            using (var package = new ExcelPackage())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    package.Load(stream);
+                }
+
+                var workSheet = package.Workbook.Worksheets.First();
+                DataTable dt = new DataTable();
+
+                foreach (var firstRowCell in workSheet.Cells[1, 1, 1, 5])
+                {
+                    dt.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                }
+                var startRow = hasHeader ? 2 : 1;
+                for (int rowNum = startRow; rowNum <= 45; rowNum++)
+                {
+                    var wsRow = workSheet.Cells[rowNum, 1, rowNum, 5];
+                    DataRow row = dt.Rows.Add();
+                    foreach (var cell in wsRow)
+                    {
+                        row[cell.Start.Column - 1] = cell.Text;
+                    }
+                }
+                return dt;
+            }
+        }
+    }
+}
---
+++ CorcoranExercise/Repositories/Implementations/PresidentRepository.cs
@@ -0,0 +1,103 @@
+﻿using CorcoranExercise.Models;
+using CorcoranExercise.Models.Entities;
+using CorcoranExercise.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CorcoranExercise.Utilities;
+using CorcoranExercise.Models.ViewModel;
+
+namespace CorcoranExercise.Repositories.Implementations
+{
+    public class PresidentRepository : IPresidentRepository
+    {
+        private PresidentServiceContext context;
+
+        public PresidentRepository()
+        {
+            context = new PresidentServiceContext();
+        }
+
+        public List<President> GetAll()
+        {
+            try
+            {
+                List<President> list = new List<President>();
+
+                foreach (System.Data.DataTable table in context.DtSet.Tables)
+                {
+                    foreach (System.Data.DataRow row in table.Rows)
+                    {
+                        //Creates the president object
+                        var president = new President()
+                        {
+                            Name = row["President"].ToString(),
+                            Birthday = Convert.ToDateTime(row["Birthday"]),
+                            Birthplace = row["Birthplace"].ToString(),
+                            Deathday = row["Death day"].ToString() == "" ? (DateTime?)null : Convert.ToDateTime(row["Death day"]),
+                            Deathplace = row["Death place"].ToString()
+                        };
+
+                        list.Add(president);
+                    }
+                }
+
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<President> Get(string name)
+        {
+            try
+            {
+                var list = GetAll();
+
+                return list.Where(x => x.Name.Contains(name)).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<President> ChangeOrder(SortVM viewModel)
+        {
+            try
+            {
+                var list = GetAll();
+
+                switch (viewModel.Name)
+                {
+                    case ColumnName.BirthDay:
+                        if (viewModel.IsAscending)
+                            return list.OrderBy(x => !x.Deathday.HasValue)
+                                .ThenBy(x => x.Birthday)
+                                .ToList();
+                        else
+                            return list.OrderByDescending(x => x.Deathday.HasValue)
+                                .ThenByDescending(x => x.Birthday)
+                                .ToList();
+
+                    case ColumnName.DeathDay:
+                        if (viewModel.IsAscending)
+                            return list.OrderBy(x => !x.Deathday.HasValue)
+                                .ToList();
+                        else
+                            return list.OrderByDescending(x => x.Deathday.HasValue)
+                                .ThenByDescending(x => x.Deathday)
+                                .ToList();
+                    default:
+                        throw new Exception("Wrong Column Name");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
